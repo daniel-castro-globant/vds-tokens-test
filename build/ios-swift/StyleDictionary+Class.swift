bfd7d00354d17c3d57e6e14f294c5d072d8e60e8
Daniel Castro <johndaniel.castro@globant.com>
2022-07-18 15:47:24 -0500

    Run style-dictionary

--- build/ios-swift/StyleDictionary+Class.swift
+++ build/ios-swift/StyleDictionary+Class.swift
@@ -4,44 +4,17 @@
 //
 
 // Do not edit directly
-// Generated on Mon, 18 Jul 2022 19:34:47 GMT
+// Generated on Mon, 18 Jul 2022 20:45:16 GMT
 
 
 import UIKit
 
 public class StyleDictionaryClass {
-    public static let colorBaseGrayDark = UIColor(red: 0.067, green: 0.067, blue: 0.067, alpha: 1)
-    public static let colorBaseGrayLight = UIColor(red: 0.800, green: 0.800, blue: 0.800, alpha: 1)
-    public static let colorBaseGrayMedium = UIColor(red: 0.600, green: 0.600, blue: 0.600, alpha: 1)
-    public static let colorBaseGreen = UIColor(red: 0.000, green: 1.000, blue: 0.000, alpha: 1)
-    public static let colorBaseRed = UIColor(red: 1.000, green: 0.000, blue: 0.000, alpha: 1)
-    public static let colorColorCelestialSphere = UIColor(red: 0.400, green: 0.400, blue: 0.400, alpha: 1)
-    public static let colorColorPDark = UIColor(red: 0.000, green: 0.216, blue: 0.773, alpha: 1)
-    public static let colorColorPGradient1 = UIColor(red: 0.000, green: 0.000, blue: 0.000, alpha: 1)
-    public static let colorColorPGradient2 = UIColor(red: 0.000, green: 0.000, blue: 0.000, alpha: 1)
-    public static let colorColorSingularity = UIColor(red: 0.114, green: 0.114, blue: 0.114, alpha: 1)
-    public static let colorColorSnowwhite = UIColor(red: 1.000, green: 1.000, blue: 1.000, alpha: 1)
-    public static let colorFontBase = UIColor(red: 1.000, green: 0.000, blue: 0.000, alpha: 1)
-    public static let colorFontSecondary = UIColor(red: 0.000, green: 1.000, blue: 0.000, alpha: 1)
-    public static let colorFontTertiary = UIColor(red: 0.800, green: 0.800, blue: 0.800, alpha: 1)
-    public static let sizeFontBase = CGFloat(16.00) /* the base size of the font */
-    public static let sizeFontLarge = CGFloat(32.00) /* the large size of the font */
-    public static let sizeFontMedium = CGFloat(16.00) /* the medium size of the font */
-    public static let sizeFontSmall = CGFloat(12.00) /* the small size of the font */
-    public static let spacing1100 = 40px
-    public static let spacing400 = 12px
-    public static let spacing500 = 20px
-    public static let spacing600 = 20px
-    public static let typographyComponentsButtonLargeFontFamily = Proxima Nova A
-    public static let typographyComponentsButtonLargeFontSize = 15
-    public static let typographyComponentsButtonLargeFontWeight = Semibold
-    public static let typographyFontFamiliesProximaNovaA = Proxima Nova A
-    public static let typographyFontSize0 = 12
-    public static let typographyFontSize1 = 14
-    public static let typographyFontSize2 = 15
-    public static let typographyFontWeightsProximaNovaA0 = Semibold
-    public static let typographyLetterSpacing0 = 0.46
-    public static let typographyLetterSpacing1 = 0.4
-    public static let typographyLineHeights0 = 16
-    public static let typographyParagraphSpacing0 = 0
+    public static let vdsColorCelestialsphere = #666666
+    public static let vdsColorPDark = #0037c5
+    public static let vdsColorPGradient1 = linear-gradient(225deg, #0064ff 0%, #0037c5 100%)
+    public static let vdsColorPGradient2 = linear-gradient(45deg, #0064ff 0%, #0037c5 100%)
+    public static let vdsColorPLight = #0064ff
+    public static let vdsColorSingularity = #1d1d1d
+    public static let vdsColorSnowwhite = #ffffff
 }
--- build/ios-swift/StyleDictionary+Struct.swift
+++ build/ios-swift/StyleDictionary+Struct.swift
@@ -4,44 +4,17 @@
 //
 
 // Do not edit directly
-// Generated on Mon, 18 Jul 2022 19:34:47 GMT
+// Generated on Mon, 18 Jul 2022 20:45:16 GMT
 
 
 import UIKit
 
 internal struct StyleDictionaryStruct {
-    internal static let colorBaseGrayDark = UIColor(red: 0.067, green: 0.067, blue: 0.067, alpha: 1)
-    internal static let colorBaseGrayLight = UIColor(red: 0.800, green: 0.800, blue: 0.800, alpha: 1)
-    internal static let colorBaseGrayMedium = UIColor(red: 0.600, green: 0.600, blue: 0.600, alpha: 1)
-    internal static let colorBaseGreen = UIColor(red: 0.000, green: 1.000, blue: 0.000, alpha: 1)
-    internal static let colorBaseRed = UIColor(red: 1.000, green: 0.000, blue: 0.000, alpha: 1)
-    internal static let colorColorCelestialSphere = UIColor(red: 0.400, green: 0.400, blue: 0.400, alpha: 1)
-    internal static let colorColorPDark = UIColor(red: 0.000, green: 0.216, blue: 0.773, alpha: 1)
-    internal static let colorColorPGradient1 = UIColor(red: 0.000, green: 0.000, blue: 0.000, alpha: 1)
-    internal static let colorColorPGradient2 = UIColor(red: 0.000, green: 0.000, blue: 0.000, alpha: 1)
-    internal static let colorColorSingularity = UIColor(red: 0.114, green: 0.114, blue: 0.114, alpha: 1)
-    internal static let colorColorSnowwhite = UIColor(red: 1.000, green: 1.000, blue: 1.000, alpha: 1)
-    internal static let colorFontBase = UIColor(red: 1.000, green: 0.000, blue: 0.000, alpha: 1)
-    internal static let colorFontSecondary = UIColor(red: 0.000, green: 1.000, blue: 0.000, alpha: 1)
-    internal static let colorFontTertiary = UIColor(red: 0.800, green: 0.800, blue: 0.800, alpha: 1)
-    internal static let sizeFontBase = CGFloat(16.00) /* the base size of the font */
-    internal static let sizeFontLarge = CGFloat(32.00) /* the large size of the font */
-    internal static let sizeFontMedium = CGFloat(16.00) /* the medium size of the font */
-    internal static let sizeFontSmall = CGFloat(12.00) /* the small size of the font */
-    internal static let spacing1100 = 40px
-    internal static let spacing400 = 12px
-    internal static let spacing500 = 20px
-    internal static let spacing600 = 20px
-    internal static let typographyComponentsButtonLargeFontFamily = Proxima Nova A
-    internal static let typographyComponentsButtonLargeFontSize = 15
-    internal static let typographyComponentsButtonLargeFontWeight = Semibold
-    internal static let typographyFontFamiliesProximaNovaA = Proxima Nova A
-    internal static let typographyFontSize0 = 12
-    internal static let typographyFontSize1 = 14
-    internal static let typographyFontSize2 = 15
-    internal static let typographyFontWeightsProximaNovaA0 = Semibold
-    internal static let typographyLetterSpacing0 = 0.46
-    internal static let typographyLetterSpacing1 = 0.4
-    internal static let typographyLineHeights0 = 16
-    internal static let typographyParagraphSpacing0 = 0
+    internal static let vdsColorCelestialsphere = #666666
+    internal static let vdsColorPDark = #0037c5
+    internal static let vdsColorPGradient1 = linear-gradient(225deg, #0064ff 0%, #0037c5 100%)
+    internal static let vdsColorPGradient2 = linear-gradient(45deg, #0064ff 0%, #0037c5 100%)
+    internal static let vdsColorPLight = #0064ff
+    internal static let vdsColorSingularity = #1d1d1d
+    internal static let vdsColorSnowwhite = #ffffff
 }
